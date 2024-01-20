d90a63408ecf7b074372f99c9910b2d2d29b9a20
Geovane <geovane.saraiva.silva75@gmail.com>
2024-01-20 14:35:28 -0300

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,26 @@ import Card from './components/layout/Card'
 
 export default function App() {
     return (
-        <>
+        <div className='App'>
             <h1 className='Title'>Fundamentos React</h1>
 
-            <Card titulo="Desafio: Aleatório">
-                <Aleatorio min={1} max={100} />
-            </Card>
+            <div className='Cards'>
+                <Card titulo="Desafio: Aleatório">
+                    <Aleatorio min={1} max={100} />
+                </Card>
 
-            <Card titulo="React Fragment">
-                <Fragmento />
-            </Card>
+                <Card titulo="React Fragment">
+                    <Fragmento />
+                </Card>
 
-            <Card titulo="Componente com Parâmetros">
-                <ComParametro titulo="Segundo Componente!" subtitulo="Muito Legal!" />
-            </Card>
+                <Card titulo="Componente com Parâmetros">
+                    <ComParametro titulo="Segundo Componente!" subtitulo="Muito Legal!" />
+                </Card>
 
-            <Card titulo="Primeiro Componenete">
-                <Primeiro />
-            </Card>
-
-        </>
+                <Card titulo="Primeiro Componenete">
+                    <Primeiro />
+                </Card>
+            </div>
+        </div>
     )
 }
